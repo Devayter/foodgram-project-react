cce8bca508064288dff8614d9033774a801c4ab4
Рябов Павел <ryabovpavel@Rabovs-MacBook-Air.local>
2023-11-12 23:30:45 +0300

    Ревью №2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -91,21 +91,15 @@ class RecipeSerializer(serializers.ModelSerializer):
     def get_is_favorited(self, obj):
         request = self.context.get('request')
         return (
-            request.user.is_authenticated
-            and Favorites.objects.filter(
-                recipe=obj,
-                user=request.user
-                ).exists()
+            request.user.is_authenticated and
+            Favorites.objects.filter(recipe=obj, user=request.user).exists()
         )
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
         return (
-            request.user.is_authenticated
-            and ShoppingCart.objects.filter(
-                recipe=obj,
-                user=request.user
-                ).exists()
+            request.user.is_authenticated and
+            ShoppingCart.objects.filter(recipe=obj, user=request.user).exists()
         )
 
     def to_representation(self, instance):
