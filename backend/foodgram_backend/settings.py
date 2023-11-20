f0b8d813ed4eadc6a07656edd4af4ad4f5e9215c
Рябов Павел <ryabovpavel@Rabovs-MacBook-Air.local>
2023-11-20 13:06:45 +0300

    Доки

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = ['https://foodgramforyou.ddns.net', 'http://foodgramforyou.ddns.net']
 
 CORS_URLS_REGEX = r'^/api/.*$'
 
@@ -106,7 +106,7 @@ DJOSER = {
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = '/media'
 
 
 AUTH_USER_MODEL = 'users.User'
